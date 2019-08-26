cf3500b580b75dc273fb29c3977a501c17dfbc7e
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-08-26 18:26:43 +0530

    added basic example with square and circle nodes

--- manimnx.py
+++ manimnx.py
@@ -13,13 +13,15 @@ class ManimNX(VGroup):
         self.graph = graph
         self.get_edge = get_edge
         self.get_node = get_node
-        self.add_edges()
         self.add_nodes()
+        self.add_edges()
 
     def add_nodes(self):
         for node in self.graph.nodes:
-            self.get_node(node)
+            n = self.get_node(self.graph.node[node])
+            self.add(n)
 
     def add_edges(self):
         for n1, n2 in self.graph.edges:
-            self.get_edge(n1, n2)
+            e = self.get_edge(self.graph.node[n1], self.graph.node[n2])
+            self.add_to_back(e)
