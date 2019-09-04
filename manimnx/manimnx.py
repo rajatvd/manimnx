223195b34e8378ff93677d6fccdf0a6fbbbc27dd
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-09-04 23:09:24 +0530

    shifted transform graph from factor graphs to manim-nx

--- manimnx/manimnx.py
+++ manimnx/manimnx.py
@@ -129,3 +129,168 @@ class ManimGraph(VGroup):
             self.id_to_edge[self.count] = edge
             self.add_to_back(e)
             self.count += 1
+
+
+# %%
+
+
+class TransformAndRemoveSource(Transform):
+    def clean_up_from_scene(self, scene):
+        super().clean_up_from_scene(scene)
+        scene.remove(self.mobject)
+
+# TODO: move this to manimnx package
+# %% TODO: take in custom transform for edges and nodes
+
+
+def transform_graph(mng, G):
+    """Transforms the graph in ManimGraph mng to the graph G.
+
+    This is better than just using Transform because it keeps edges and
+    nodes which don't change stationary, unlike Transform which mixes up
+    everything.
+
+    For any new mob_ids in G, or any nodes or edges without a mob_id, they
+    are assumed to be new objects which are to be created. If they contain
+    an expansion attribute, they will be created and transformed from those
+    mob_ids in the expansion. Otherwise, they will be faded in.
+
+    Missing mob_ids in G are dealt with in two ways:
+    If they are found in the contraction attribute of the node or edge, the
+    mobjects are first transformed to the contracted node then faded away. If
+    not found in any contraction, they are faded away. (Note that this means
+    that any older contractions present in G do not affect the animation in any
+    way.)
+
+    Cannot contract to new nodes/edges and expand from new nodes/edges.
+    # TODO: consider changing this behavior
+
+
+    Parameters
+    ----------
+    mng : ManimGraph
+        The ManimGraph object to transform.
+    G : Graph
+        The graph containing attributes of the target graph.
+
+    Returns
+    -------
+    list of Animations
+        List of animations to show the transform.
+
+    """
+    anims = []
+    id_to_mobj = {**mng.nodes, **mng.edges}
+
+    old_ids = list(mng.nodes.keys()) + list(mng.edges.keys())
+    new_ids = []
+
+    # just copying the loops for edges and nodes, not worth functioning that
+    # i think
+    # ------- ADDITIONS AND DIRECT TRANSFORMS ---------
+    # NODES
+    for node, node_data in G.nodes.items():
+        new_node = mng.get_node(node, G)
+        if 'mob_id' not in node_data.keys():
+            G.nodes[node]['mob_id'] = mng.count
+            mng.count += 1
+
+        mob_id = node_data['mob_id']
+        new_ids.append(mob_id)
+
+        if mob_id in old_ids:
+            # if mng.graph.nodes[mng.id_to_node[mob_id]] != node_data:
+            anims.append(Transform(mng.nodes[mob_id], new_node))
+        else:
+            if 'expansion' in node_data.keys():
+                objs = [id_to_mobj[o['mob_id']]
+                        for o in node_data['expansion'].values()]
+                anims.append(TransformFromCopy(VGroup(*objs), new_node))
+            else:
+                anims.append(FadeIn(new_node))
+
+            mng.nodes[mob_id] = new_node
+            mng.add(new_node)
+            mng.id_to_node[mob_id] = node
+
+    # EDGES
+    for edge, edge_data in G.edges.items():
+        new_edge = mng.get_edge(edge, G)
+        if 'mob_id' not in edge_data.keys():
+            G.edges[edge]['mob_id'] = mng.count
+            mng.count += 1
+
+        mob_id = edge_data['mob_id']
+        new_ids.append(mob_id)
+
+        if mob_id in old_ids:
+            # only transform if new is different from old
+            # TODO: how to check this properly, and is it really needed?
+
+            # if mng.graph.edges[mng.id_to_edge[mob_id]] != edge_data:
+            anims.append(Transform(mng.edges[mob_id], new_edge))
+        else:
+            if 'expansion' in edge_data.keys():
+                objs = [id_to_mobj[o['mob_id']]
+                        for o in edge_data['expansion'].values()]
+                anims.append(TransformFromCopy(VGroup(*objs), new_edge))
+            else:
+                anims.append(FadeIn(new_edge))
+
+            mng.edges[mob_id] = new_edge
+            mng.add_to_back(new_edge)
+            mng.id_to_edge[mob_id] = edge
+
+    # --------- REMOVALS AND CONTRACTIONS ----------
+    # NODES
+    for node, node_data in mng.graph.nodes.items():
+        mob_id = node_data['mob_id']
+        if mob_id in new_ids:
+            continue
+
+        contracts_to = []
+        for node2, node_datfg in G.nodes.items():
+            for c2 in node_datfg.get('contraction', {}).values():
+                if mob_id == c2['mob_id']:
+                    contracts_to.append(id_to_mobj[node_datfg['mob_id']])
+                    break
+
+        mobj = id_to_mobj[mob_id]
+        if len(contracts_to) == 0:
+            anims.append(FadeOut(mobj))
+        else:
+            # anims.append(Succession(Transform(mobj, VGroup(*contracts_to)),
+            #                         FadeOut(mobj)))
+            anims.append(TransformAndRemoveSource(mobj, VGroup(*contracts_to)))
+
+        # dont actually remove so that the edges in the back remain in the
+        # back while transforming. this might bite later though
+        # mng.remove(mobj)
+        del mng.nodes[mob_id]
+        del mng.id_to_node[mob_id]
+
+    # EDGES
+    for edge, edge_data in mng.graph.edges.items():
+        mob_id = edge_data['mob_id']
+        if mob_id in new_ids:
+            continue
+
+        contracts_to = []
+        for edge2, edge_datfg in G.edges.items():
+            for c2 in edge_datfg.get('contraction', {}).values():
+                if mob_id == c2['mob_id']:
+                    contracts_to.append(id_to_mobj[edge_datfg['mob_id']])
+                    break
+
+        mobj = id_to_mobj[mob_id]
+        if len(contracts_to) == 0:
+            anims.append(FadeOut(mobj))
+        else:
+            anims.append(TransformAndRemoveSource(mobj, VGroup(*contracts_to)))
+
+        # mng.remove(mobj)
+        del mng.edges[mob_id]
+        del mng.id_to_edge[mob_id]
+
+    mng.graph = G
+    return anims
